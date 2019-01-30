ae3c41bdc976e189c3872d641573e6e73dd8c6a2
hemadevi-nyros <40348452+hemadevi-nyros@users.noreply.github.com>
2019-01-30 10:15:26 +0530

    Rspec test frame work

--- spec/controllers/dishes_controller_spec.rb
+++ spec/controllers/dishes_controller_spec.rb
@@ -2,28 +2,28 @@ require 'rails_helper'
 
 RSpec.describe DishesController, type: :controller do
   context 'GET #index' do
- 	 it 'returns a success response' do
- 		 get :index
- 		 expect(response).to be_success 
- 	 end
+ 	  it 'returns a success response' do
+ 		  get :index
+ 		  expect(response).to be_success 
+ 	  end
   end
   context 'GET #search' do
- 	 it 'returns a success response' do
- 	 	 get :search
- 	 	 expect(response).to be_success
- 	 end
+ 	  it 'returns a success response' do
+ 	 	  get :search
+ 	 	  expect(response).to be_success
+ 	  end
   end
   describe 'GET#index' do
-   it "renders the :index view" do
-  	 get :index
-  	 expect(response).to render_template :index
-   end
+    it "renders the :index view" do
+  	  get :index
+  	  expect(response).to render_template :index
+    end
   end
   describe 'GET #search' do
-   it "renders dishes search page" do
-  	 get :search
-  	 expect(response).to render_template :search
-   end
+    it "renders dishes search page" do
+  	  get :search
+  	  expect(response).to render_template :search
+    end
   end
 end
  
