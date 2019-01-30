b03574d495b3d1e62ebe5e2c5161aebc5ccd3701
hemadevi-nyros <40348452+hemadevi-nyros@users.noreply.github.com>
2019-01-30 10:13:08 +0530

    Update dishes_controller_spec.rb

--- spec/controllers/dishes_controller_spec.rb
+++ spec/controllers/dishes_controller_spec.rb
@@ -7,26 +7,23 @@ RSpec.describe DishesController, type: :controller do
  		 expect(response).to be_success 
  	 end
   end
-  
   context 'GET #search' do
  	 it 'returns a success response' do
  	 	 get :search
  	 	 expect(response).to be_success
  	 end
- 	end
-
+  end
   describe 'GET#index' do
    it "renders the :index view" do
   	 get :index
   	 expect(response).to render_template :index
    end
   end
-
   describe 'GET #search' do
-  	it "renders dishes search page" do
-  		get :search
-  		expect(response).to render_template :search
-  	end
+   it "renders dishes search page" do
+  	 get :search
+  	 expect(response).to render_template :search
+   end
   end
 end
  
