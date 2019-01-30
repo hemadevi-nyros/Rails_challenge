d5e381a06ce7339b2e7ad7b2377ee57cac0a895f
hemadevi-nyros <40348452+hemadevi-nyros@users.noreply.github.com>
2019-01-30 10:00:52 +0530

    Update dishes_controller_spec.rb

--- spec/controllers/dishes_controller_spec.rb
+++ spec/controllers/dishes_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe DishesController, type: :controller do
-	context 'GET #index' do
+  context 'GET #index' do
  	 it 'returns a success response' do
  		 get :index
  		 expect(response).to be_success 
